10f70d9170a0393aceece17cff40b45fb45c5707
khimnguynn <khimnguynn@gmail.com>
2022-11-10 13:23:09 +0700

    edit version

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return 'Hello World v1'
+    return 'Hello World v2'
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=2222)
